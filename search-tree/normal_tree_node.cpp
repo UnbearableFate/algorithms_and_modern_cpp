c774cf77d8a217c86564e18472e6cd2a63f00be4
UnbearableFate <tylov.ohno@gmail.com>
2020-09-07 01:59:31 +0000

    change file name

---
+++ search-tree/normal_tree_node.cpp
@@ -0,0 +1,9 @@
+#include "normal_tree_node.h"
+
+template<typename T>
+using p2n = shared_ptr<normal_tree_node<T>>; // means pointer to tree node;
+
+template<typename T>
+shared_ptr<normal_tree_node<T>> baseTree<T>::insert(int key, T value) {
+    w
+}
